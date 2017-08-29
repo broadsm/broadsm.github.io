636f9dfbce7d117fe85f5b9b8f5e29e18766aa6e
Broadway <broadways@dot.state.co.us>
2017-08-29 09:04:53 -0600

    qcew project updates

---
+++ Projects/QCEW/js/gallery.js
@@ -0,0 +1,14 @@
+<script>
+    $(document).ready(function() {
+      // This will create a single gallery from all elements that have class "gallery-item"
+        $('.gallery-item').magnificPopup({
+          type: 'image',
+          gallery:{
+            enabled:true
+          }
+        });
+        
+        // MixItUp 2
+        $('#container').mixItUp();
+    });
+</script>
